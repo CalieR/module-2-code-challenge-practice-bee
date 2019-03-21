99499954fd22fe33f0025a9ae76d87a1d1d70299
CalieR <calielrushton@gmail.com>
2019-03-21 14:15:51 +0000

    strange issue with association methods

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,3 +1,28 @@
 class FlowersController < ApplicationController
 
+    def index 
+        @flowers = Flower.all 
+    end
+    
+    def show
+        @flower = Flower.find(params[:id])
+    end
+
+    def new
+        @flower = Flower.new
+        @bees = Bee.all
+    end
+
+    def create 
+        @flower = Flower.create(flower_params)
+        if @flower.valid?
+            redirect_to flower_path
+        end
+    end
+
+    private
+
+    def flower_params
+        params.require(:flower).permit(:name, :pollen_capacity, :bee_ids [])
+    end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+    belongs_to :bees 
+    belongs_to :flowers
 end
